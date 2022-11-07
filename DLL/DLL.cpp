a2ee84ff8c3b24e1161c3f83badf84ffdc5ccfda
полковниковаав <@101-07>
2022-11-07 16:25:36 +0300

    Добавьте файлы проекта.

---
+++ DLL/DLL.cpp
@@ -0,0 +1,14 @@
+﻿#include <windows.h>
+#define PATH L"DLLCode.dll"
+typedef int(_cdecl* MyFunction)(LPWSTR);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HINSTANCE hMyDLL;
+	if ((hMyDLL = LoadLibrary(PATH)) == NULL) return 1;
+	MyFunction myFun = (MyFunction)GetProcAddress(hMyDLL, "MyFunc");
+	myFun(L"Все работает?");
+
+	FreeLibrary(hMyDLL);
+	return 0;
+}
